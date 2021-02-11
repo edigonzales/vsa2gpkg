3368716c87be68829837c8d23110645e6da02371
Stefan Ziegler <edi.gonzales@gmail.com>
2021-02-11 20:29:18 +0100

    initial commit

---
+++ src/main/java/ch/so/agi/vsa2gpkg/Vsa2gpkgApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi.vsa2gpkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vsa2gpkgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vsa2gpkgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/so/agi/vsa2gpkg/Vsa2gpkgApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.so.agi.vsa2gpkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vsa2gpkgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
